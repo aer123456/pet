461eea1f2f05c0aa319c99cec1b5c11ef81dd6f9
huguantao <huguantao@kuaishou.com>
2020-05-27 00:10:07 +0800

    保本分类页面接口

--- pages/sortList/sortList.js
+++ pages/sortList/sortList.js
@@ -3,9 +3,9 @@
 * @author: huguantao
 * @Date: 2020-05-19 21:02:34
 * @LastEditors: huguantao
-* @LastEditTime: 2020-05-24 21:18:27
+* @LastEditTime: 2020-05-26 23:53:00
  */
-// pages/sortList/sortList.js
+import {doRequest} from '../../utils/util';
 const App = getApp();
 
 Page({
@@ -14,14 +14,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    navH: App.globalData.navHeight
+    navH: App.globalData.navHeight,
+    sortList: [],     // 左侧导航栏数据
+    currentId: '',    // 导航栏当前选中
+    currentList: [],  // 右侧列表
+    pageNo: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const param = {
+      url: '/shop/category',
+      method: 'GET',
+      data: { a: 1}
+    }
+    doRequest(param).then((res) => {
+      this.setData({
+        sortList: res.data.list,
+        currentId: res.data.list[0].id
+      });
 
+      if(res.data.list.length > 0) {
+        this.getPetList(res.data.list[0].id, this.data.pageNo)
+      } else {
+        // TODO 暂无数据
+      }
+    });
   },
 
   /**
@@ -71,5 +91,30 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  getPetList: function(cid, page) {
+    const param = {
+      url: `/shop/shopList?cid=${cid}&page=${page}`,
+      method: 'GET',
+      data: {}
+    }
+    doRequest(param).then((res) => {
+      this.setData({currentList: this.data.currentList.concat(res.data.list)});
+    });
+  },
+
+  switchList: function(event) {
+    var id = event.currentTarget.dataset.id;
+    this.setData({
+      currentId: id,
+      pageNo: 0,
+      currentList: []
+    });
+    this.getPetList(id, 0)
+  },
+  gotoBuy: function(event) {
+    var id = event.currentTarget.dataset.id;
+    wx.navigateTo({url: `/pages/petDetail/petDetail?id=${id}`})
+  },
 })
